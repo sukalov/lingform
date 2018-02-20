a2f7a8d365d9e3663c97189ee4928a964e57b2ef
Matvey Sokolovskiy <matvey10@gmail.com>
2018-02-20 10:48:37 +0300

    first copy from flask_project

---
+++ form.py
@@ -0,0 +1,11 @@
+import json
+from flask import Flask
+from flask import url_for, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
