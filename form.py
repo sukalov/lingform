0c1ed6f0bb7d225c39733515b721840d324d49d8
Matvey Sokolovskiy <matvey10@gmail.com>
2018-03-19 20:42:52 +0300

    heroku requirements

--- form.py
+++ form.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import url_for, render_template, request, redirect
 import random
-import re
 
 def get_file_str(file):
     file = open(file, 'r', encoding='utf-8')
@@ -18,15 +17,23 @@ def randomize_stimulus_in_html(lang):
     html = get_file_str('templates/' + lang + '_template.html')
 
     numbers = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16"]
+    imgorder = ''
 
     for i in range(1,17):
         template = "N" + str(i) + "N"
         a = random.choice(numbers)
         numbers.remove(a)
         html = html.replace(template, a)
+        if numbers != []:
+            imgorder = imgorder + a + ','
+        else:
+            imgorder = imgorder + a
 
     write_txt(html, "templates/" + lang + "_pictures.html")
 
+    results = open("results.tsv", 'a', encoding='utf-8')
+    results.write(imgorder + '\t')
+    results.close()
 
 app = Flask(__name__)
 
@@ -407,4 +414,7 @@ def eng_finish():
     return render_template('en_finish.html')
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
